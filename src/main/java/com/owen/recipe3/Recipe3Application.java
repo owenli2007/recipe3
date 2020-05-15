460debe6f6d766224d8245a4fcade55b2c28ed6b
Owen Li <owenli2007@hotmail.com>
2020-05-15 12:41:12 +0800

    # WARNING: head commit changed in the meantime
    init commit

---
+++ src/main/java/com/owen/recipe3/Recipe3Application.java
@@ -0,0 +1,13 @@
+package com.owen.recipe3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipe3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Recipe3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/owen/recipe3/Recipe3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owen.recipe3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
